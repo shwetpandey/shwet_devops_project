9d5c2fb2c6cc6afc3aafdd0cc40ce3e52cc7733f
shwetpandey <shwetpandey41@gmail.com>
2024-05-24 16:39:12 +0530

    to provide the access to port 80 to open jenkins in browser

--- terraform_code/sp-EC2-vpc-for-each.tf
+++ terraform_code/sp-EC2-vpc-for-each.tf
@@ -30,6 +30,14 @@ resource "aws_security_group" "sp-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     }
 
+  ingress {
+    description      = "to run jenkins on browser"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
   egress {
     from_port        = 0
     to_port          = 0
